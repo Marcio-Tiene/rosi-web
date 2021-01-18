61e0b23ad1db6798ffe395cc09db66a5fc97899b
Marcio Tiene <marciorft@gmail.com>
2021-01-18 15:59:53 -0300

    test 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,13 +40,14 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="manifest" href="./manifest.json" />
-          <link rel="icon" href="./favicon.ico" />
           <meta name="Description" content="Rosi Job web page" />
-          <link rel="icon" href="./rosijob-logo-32x32.png" />
-          <link rel="icon" href="./rosijob-logo-192x192.png" />
-          <link rel="apple-touch-icon" href="./rosijob-logo-180x180.png" />
-          <meta name="msapplication-TileImage" content="./rosijob-logo-270x270.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
