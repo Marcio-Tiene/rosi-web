3b71eb8a15e6512abe50dfd2ef74462ef4b5269d
Marcio Tiene <marciorft@gmail.com>
2021-01-19 11:23:15 -0300

    icon test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -46,6 +46,8 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta property="og:title" content="The Rock" />
+          <meta property="og:image" content="/favicon-32x32.png" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
