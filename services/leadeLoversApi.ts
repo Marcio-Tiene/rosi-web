ba1673b22eca69225eaef1a006556e2e8575ce4b
Marcio Tiene <marciorft@gmail.com>
2021-01-21 00:54:48 -0300

    https included

--- services/leadeLoversApi.ts
+++ services/leadeLoversApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const leadLoversapi = axios.create({
-  baseURL: 'http://llapi.leadlovers.com/webapi',
+  baseURL: 'https://llapi.leadlovers.com/webapi',
 });
 
 export default leadLoversapi;
