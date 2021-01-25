50cbb7ab51b50baac29f5ca03cce36a03b459183
Marcio Tiene <marciorft@gmail.com>
2021-01-25 11:15:13 -0300

    forn respossivness adujus

--- components/LandingForm/styles.tsx
+++ components/LandingForm/styles.tsx
@@ -7,7 +7,7 @@ export const Form = styled(Unform)`
   /* margin-right: 45px; */
   /* margin-top: 200px; */
   width: 330px;
-  max-width: 90vw;
+  max-width: 80vw;
   margin-bottom: 30px;
   margin-top: 20px;
 
